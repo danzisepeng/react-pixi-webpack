c6a74315fbd3cf923c077ec13b0bf22977128ee7
Zhang Zhipeng <414326615@qq.com>
2018-01-05 19:14:03 +0800

    increase get IPv4 method add to devServer host

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OS = require('os');
 
 const webpackConfig = {
   entry: {},
@@ -14,8 +15,9 @@ const webpackConfig = {
   //设置开发者工具的端口号,不设置则默认为8080端口
   devServer: {
     inline: true,
-    port: 8181
+    port: 3000
   },
+  devtool: 'source-map',
   module:{
     rules:[
       {
@@ -37,7 +39,7 @@ const webpackConfig = {
             loader: 'file-loader',
             options: {
               outputPath: './images/',
-              publicPath: '.'
+              publicPath: ''
             }
           }
         ]
@@ -56,6 +58,19 @@ const webpackConfig = {
     )
   ],
 };
+//动态获取本机IPv4配置devserver.host
+
+const NetWork = OS.networkInterfaces();
+
+for ( let item in NetWork ) {
+  const devName = NetWork[item];
+  for ( let i = 0; i < devName.length; i++) {
+    const oKeys = devName[i];
+    if (oKeys.family === "IPv4" && oKeys.address !== "127.0.0.1" && !oKeys.internal) {
+      webpackConfig.devServer.host = oKeys.address;
+    }
+  }
+}
 
 // 获取指定路径下的入口文件
 function getEntries(globPath) {
